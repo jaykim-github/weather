2bce0da744a25697d30e9666e762aa14db6868c5
jaykim-github <jaykeem713@gmail.com>
2022-11-28 01:52:27 +0900

    [feat] 2022.11.28 : 받아온 날씨 json 파싱하기

--- src/main/java/com/zerobase/weather/service/DiaryService.java
+++ src/main/java/com/zerobase/weather/service/DiaryService.java
@@ -1,5 +1,8 @@
 package com.zerobase.weather.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -15,39 +20,60 @@ public class DiaryService {
     @Value("${openweathermap.key}")
     private String apikey;
 
-    public void createDiary(LocalDateTime date, String text){
+    public void createDiary(LocalDateTime date, String text) {
         //open weather map에서 날씨 데이터 가져오기
         String weatherData = getWeatherString();
+
+        //받아온 날씨 json 파싱하기
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
     }
 
-    private String getWeatherString(){
-       String apiUrl =
-        "https://api.openweathermap.org/data/2.5/weather?lat=37.3900688&lon=127.1140152&appid=" + apikey;
-       try{
-           URL url = new URL(apiUrl);
-           HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-           connection.setRequestMethod("GET");
-           int responseCode = connection.getResponseCode();
-           BufferedReader br;
-           if(responseCode == 200){
-               br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-           }else{
-               br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-           }
+    private String getWeatherString() {
+        String apiUrl =
+                "https://api.openweathermap.org/data/2.5/weather?lat=37.3900688&lon=127.1140152&appid=" + apikey;
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
 
-           String inputLine;
-           StringBuilder response = new StringBuilder();
-           while((inputLine = br.readLine()) != null){
-               response.append(inputLine);
-           }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
             br.close();
 
-           return response.toString();
-       }catch (Exception e){
-           return "failed to get response";
-       }
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
 
     }
 
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
 
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("weather");
+        resultMap.put("temp",mainData.get("temp"));
+        JSONObject weatherData = (JSONObject) jsonObject.get("main");
+        resultMap.put("main",weatherData.get("main"));
+        resultMap.put("icon",weatherData.get("icon"));
+
+        return resultMap;
+    }
 }
