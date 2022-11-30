cf145de15d02cfca94c2d4845c4adb0588d43a45
jaykim-github <jaykeem713@gmail.com>
2022-12-01 00:20:56 +0900

    [feat] 2022.12.01 : swagger apidocument 적용

---
+++ src/main/java/com/zerobase/weather/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.zerobase.weather.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    //http://localhost:8080/swagger-ui/index.html 에서 api 문서를 조회할 수 있다.
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zerobase.weather"))
+                //.paths(PathSelectors.ant("/read/**")) //any는 모든 api가 나오도록 하겠다. none은 아무것도 안보여준다. ant는 특정 경로만 가능
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        String description = "Welcome Log Company";
+        return new ApiInfoBuilder()
+                .title("날씨 일기 프로젝트")
+                .description("날씨 일기를 CRUD 할 수 있는 백엔드 API입니다.")
+                .version("1.1")
+                .build();
+    }
+}
+
+
+
--- src/main/java/com/zerobase/weather/controller/DiaryController.java
+++ src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.zerobase.weather.controller;
 
 import com.zerobase.weather.domain.Diary;
 import com.zerobase.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기를 저장", notes = "value 아래에 더 긴 설명을 쓰는 공간") //api에 대한 한줄 설명
     @PostMapping("/create/diary")
     void createDiary(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
@@ -24,14 +27,17 @@ public class DiaryController {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  @ApiParam(value = "조회할 기간의 첫번째날", example = "2020-02-02")LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                            @ApiParam(value = "조회할 기간의 마지막날", example = "2020-02-02") LocalDate endDate){
         return diaryService.readDiaries(startDate,endDate);
     }
 
